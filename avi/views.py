9a76e918442d14cdfe4acb261d842a2af37ae95a
Abhishek <53704940+abhi526691@users.noreply.github.com>
2021-07-18 04:40:25 -0700

    Update views.py

--- avi/views.py
+++ avi/views.py
@@ -17,3 +17,42 @@ def index(request):
         # print(data[kw]['rising']['query'].head(50))
     context = {'li' : data['insurance']['rising']['query']}
     return render(request, 'index.html', context)
+
+def finance(request):
+    all_keywords = ['finance']
+    trendshow.build_payload(all_keywords, cat=0, timeframe='today 1-m', geo='US')
+    data = trendshow.related_queries()
+    # print(data['insurance']['rising']['query'])
+    li = []
+    for kw in all_keywords:
+        # print(data[kw]['rising']['query'].head(50))
+        li.append(data[kw]['rising']['query'])	
+        # print(data[kw]['rising']['query'].head(50))
+    context = {'li' : data['finance']['rising']['query']}
+    return render(request, 'finance.html', context)
+
+def medical(request):
+    all_keywords = ['medical']
+    trendshow.build_payload(all_keywords, cat=0, timeframe='today 1-m', geo='US')
+    data = trendshow.related_queries()
+    # print(data['insurance']['rising']['query'])
+    li = []
+    for kw in all_keywords:
+        # print(data[kw]['rising']['query'].head(50))
+        li.append(data[kw]['rising']['query'])	
+        # print(data[kw]['rising']['query'].head(50))
+    context = {'li' : data['medical']['rising']['query']}
+    return render(request, 'medical.html', context)
+
+def loan(request):
+    all_keywords = ['loan']
+    trendshow.build_payload(all_keywords, cat=0, timeframe='today 1-m', geo='US')
+    data = trendshow.related_queries()
+    # print(data['insurance']['rising']['query'])
+    li = []
+    for kw in all_keywords:
+        # print(data[kw]['rising']['query'].head(50))
+        li.append(data[kw]['rising']['query'])	
+        # print(data[kw]['rising']['query'].head(50))
+    context = {'li' : data['loan']['rising']['query']}
+    return render(request, 'loan.html', context)
