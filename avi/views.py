3ffe0fa252f7a76577c9a73f39f9c21eb05970db
Abhishek <53704940+abhi526691@users.noreply.github.com>
2021-07-18 07:19:39 -0700

    Update views.py

--- avi/views.py
+++ avi/views.py
@@ -5,54 +5,40 @@ import pandas as pd
 trendshow = TrendReq(hl='en-US', tz=360)
 # Create your views here.
 
+def index1(request):
+    all_keywords = ['insurance']
+    trendshow.build_payload(all_keywords, cat=0, timeframe='today 1-m', geo='US')
+    data = trendshow.related_queries()
+
+    for kw in all_keywords:	
+	    print(data[kw]['rising'].head(50))
+
 def index(request):
     all_keywords = ['insurance']
     trendshow.build_payload(all_keywords, cat=0, timeframe='today 1-m', geo='US')
     data = trendshow.related_queries()
-    # print(data['insurance']['rising']['query'])
-    li = []
-    for kw in all_keywords:
-        # print(data[kw]['rising']['query'].head(50))
-        li.append(data[kw]['rising']['query'])	
-        # print(data[kw]['rising']['query'].head(50))
-    context = {'li' : data['insurance']['rising']['query']}
+
+    all_keywordsFinance = ['finance']
+    trendshow.build_payload(all_keywordsFinance, cat=0, timeframe='today 1-m', geo='US')
+    data1 = trendshow.related_queries()
+
+    context = {'li' : data['insurance']['rising']['query'], 'value' : data['insurance']['rising']['value'], 'financeQuery' : data1['finance']['rising']['query'], 'financeValue' : data1['finance']['rising']['value'] }
     return render(request, 'index.html', context)
 
-def finance(request):
-    all_keywords = ['finance']
-    trendshow.build_payload(all_keywords, cat=0, timeframe='today 1-m', geo='US')
-    data = trendshow.related_queries()
-    # print(data['insurance']['rising']['query'])
-    li = []
-    for kw in all_keywords:
-        # print(data[kw]['rising']['query'].head(50))
-        li.append(data[kw]['rising']['query'])	
-        # print(data[kw]['rising']['query'].head(50))
-    context = {'li' : data['finance']['rising']['query']}
-    return render(request, 'finance.html', context)
+
+
 
 def medical(request):
     all_keywords = ['medical']
     trendshow.build_payload(all_keywords, cat=0, timeframe='today 1-m', geo='US')
     data = trendshow.related_queries()
-    # print(data['insurance']['rising']['query'])
-    li = []
-    for kw in all_keywords:
-        # print(data[kw]['rising']['query'].head(50))
-        li.append(data[kw]['rising']['query'])	
-        # print(data[kw]['rising']['query'].head(50))
-    context = {'li' : data['medical']['rising']['query']}
-    return render(request, 'medical.html', context)
 
-def loan(request):
     all_keywords = ['loan']
     trendshow.build_payload(all_keywords, cat=0, timeframe='today 1-m', geo='US')
-    data = trendshow.related_queries()
-    # print(data['insurance']['rising']['query'])
-    li = []
-    for kw in all_keywords:
-        # print(data[kw]['rising']['query'].head(50))
-        li.append(data[kw]['rising']['query'])	
-        # print(data[kw]['rising']['query'].head(50))
-    context = {'li' : data['loan']['rising']['query']}
-    return render(request, 'loan.html', context)
+    data1 = trendshow.related_queries()
+
+    context = {'li' : data['medical']['rising']['query'], 'value' : data['medical']['rising']['value'], 'loanQuery' : data1['loan']['rising']['query'], 'loanValue' : data1['loan']['rising']['value']}
+    return render(request, 'medical.html', context)
+
+
+
