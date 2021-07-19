962f167b55f6214d40652d63328d2bda4efd24f8
Abhishek <53704940+abhi526691@users.noreply.github.com>
2021-07-19 12:35:34 -0700

    Update views.py

--- avi/views.py
+++ avi/views.py
@@ -5,26 +5,26 @@ import pandas as pd
 trendshow = TrendReq(hl='en-US', tz=360)
 # Create your views here.
 
-def index1(request):
-    all_keywords = ['insurance']
-    trendshow.build_payload(all_keywords, cat=0, timeframe='today 1-m', geo='US')
-    data = trendshow.related_queries()
-
-    for kw in all_keywords:	
-	    print(data[kw]['rising'].head(50))
 
 def index(request):
     all_keywords = ['insurance']
     trendshow.build_payload(all_keywords, cat=0, timeframe='today 1-m', geo='US')
     data = trendshow.related_queries()
-
-    all_keywordsFinance = ['finance']
+    # try:
+    all_keywordsFinance = ['finance2025']
     trendshow.build_payload(all_keywordsFinance, cat=0, timeframe='today 1-m', geo='US')
     data1 = trendshow.related_queries()
-
-    context = {'li' : data['insurance']['rising']['query'], 'value' : data['insurance']['rising']['value'], 'financeQuery' : data1['finance']['rising']['query'], 'financeValue' : data1['finance']['rising']['value'] }
-    return render(request, 'index.html', context)
-
+    if data['insurance']['rising']['query'] is None:
+        if data1['finance2025']['rising'] is None:
+            return render(request, 'index.html', context={'li':[], 'value':[], 'financeQuery':[], 'financeValue':[]})
+        else:
+            return render(request, 'index.html', context={'li':[], 'value':[], 'financeQuery' : data1['finance2025']['rising']['query'], 'financeValue' : data1['finance2025']['rising']['value']})
+    else:
+        if data1['finance2025']['rising'] is None:
+            return render(request, 'index.html', {'li' : data['insurance']['rising']['query'], 'value' : data['insurance']['rising']['value'], 'financeQuery':[], 'financeValue':[]})
+        else:
+            context = {'li' : data['insurance']['rising']['query'], 'value' : data['insurance']['rising']['value'], 'financeQuery' : data1['finance2025']['rising']['query'], 'financeValue' : data1['finance2025']['rising']['value'] }
+            return render(request, 'index.html', context)
 
 
 
@@ -37,8 +37,18 @@ def medical(request):
     trendshow.build_payload(all_keywords, cat=0, timeframe='today 1-m', geo='US')
     data1 = trendshow.related_queries()
 
-    context = {'li' : data['medical']['rising']['query'], 'value' : data['medical']['rising']['value'], 'loanQuery' : data1['loan']['rising']['query'], 'loanValue' : data1['loan']['rising']['value']}
-    return render(request, 'medical.html', context)
+    if data['medical']['rising']['query'] is None:
+        if data1['loan']['rising'] is None:
+            return render(request, 'medical.html', context={'li':[], 'value':[], 'loanQuery':[], 'loanValue':[]})
+        else:
+            return render(request, 'medical.html', context={'li':[], 'value':[], 'loanQuery' : data1['loan']['rising']['query'], 'loanValue' : data1['loan']['rising']['value']})
+    else:
+        if data1['loan']['rising'] is None:
+            return render(request, 'medical.html', {'li' : data['medical']['rising']['query'], 'value' : data['medical']['rising']['value'], 'loanQuery':[], 'loanValue':[]})
+        else:
+            context = {'li' : data['medical']['rising']['query'], 'value' : data['medical']['rising']['value'], 'loanQuery' : data1['loan']['rising']['query'], 'loanValue' : data1['loan']['rising']['value'] }
+            return render(request, 'medical.html', context)
+
 
 
 
